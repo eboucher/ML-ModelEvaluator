4499fe704774d43a159fa52cb4e37afcd444f4f4
Bartholome <bartholome.lebruno@gmail.com>
2019-05-12 15:48:38 +0200

    avec moi ça marche et schmidt

--- JEEProject/src/main/java/com/example/demo/SpringbootwildflyApplication.java
+++ JEEProject/src/main/java/com/example/demo/SpringbootwildflyApplication.java
@@ -1,17 +1,26 @@
-package com.mastertheboss.springboot.springbootwildfly;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class SpringbootwildflyApplication   {
+public class SpringbootwildflyApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootwildflyApplication.class, args);
 	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<SpringbootwildflyApplication> applicationClass = SpringbootwildflyApplication.class;
 }
 
 
